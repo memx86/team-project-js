5ebd72b4d9ab30d4297950595a1613dfa18618fe
Yevhen Malyshko <memx86@gmail.com>
2022-02-23 10:45:18 +0200

    removed callback call in item initialization

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-export default function app() {}
+import Pagination from './services/pagination';
+export default function app() {
+  const pagination = new Pagination({
+    page: 10,
+    totalPages: 19,
+    callback: page => {
+      console.log(`Запрос на бекенд по странице № ${page} и рендер галлереи`);
+    },
+  });
+  pagination.showPagination();
+}
