f54246ef852c9497b68427309bc4665cd47815c9
Yevhen Malyshko <memx86@gmail.com>
2022-02-23 10:46:02 +0200

    test run clear

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,1 @@
-import Pagination from './services/pagination';
-export default function app() {
-  const pagination = new Pagination({
-    page: 10,
-    totalPages: 19,
-    callback: page => {
-      console.log(`Запрос на бекенд по странице № ${page} и рендер галлереи`);
-    },
-  });
-  pagination.showPagination();
-}
+export default function app() {}
