213e78468ba7852263b100aa6acbf794941e5914
Yevhen Malyshko <memx86@gmail.com>
2022-02-23 18:04:18 +0200

    arrows and dots

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
 import ApiTMDB from './services/apiTMDB';
 import Storage from './services/storage';
+import Pagination from './services/pagination';
 export default async function app() {
   const api = new ApiTMDB();
   const genresStorage = new Storage(Storage.KEYS.GENRES);
   const genres = await api.getGenres();
   genresStorage.save(genres);
+  const pagination = new Pagination({
+    page: 10,
+    totalPages: 19,
+    callback: page => {
+      console.log(`Запрос на бекенд по странице № ${page} и рендер галлереи`);
+    },
+  });
+  pagination.showPagination();
 }
