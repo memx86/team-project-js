82a153a81dacd5eb8907968928e7c8199f08c502
Yevhen Malyshko <memx86@gmail.com>
2022-02-23 16:59:43 +0200

    refactoring

--- src/js/searchFilms.js
+++ src/js/searchFilms.js
@@ -1,47 +1,41 @@
-// Error message: "Search result not successful. Enter the correct movie name."
-
-import renderMarkup from "./film_card";
-import ApiTMDB from "./services/apiTMDB";
-import Pagination from "./services/pagination";
-import Storage from "./services/storage";
+import renderMarkup from './film_card';
+import ApiTMDB from './services/apiTMDB';
+import Pagination from './services/pagination';
+import Storage from './services/storage';
 
 const submitForm = document.querySelector('.header-form');
 const gallery = document.querySelector('.gallery');
-
+const ERROR_MESSAGE = 'Search is not successful. Enter the correct movie name.';
 const searchApi = new ApiTMDB();
 const storage = new Storage(Storage.KEYS.MOVIES);
 
 const pagination = new Pagination({
-    callback: paginationCallback,
+  callback: paginationCallback,
 });
-
-const searchFilms = (e) => {
-    gallery.innerHTML = '';
-    const {
-    elements: { search }
-    } = e.currentTarget;
-    const value = search.value.trim();
-    if (!value) {
-        return;
-    };
-
-    searchApi.query = value;
-    
-    searchApi.searchMovies().then((data) => {
-        renderMarkup(data.results);
-        storage.save(data.results);
-        pagination.totalPages = data.total_pages;
-        pagination.showPagination();
-        return;
-    }).catch(() => "error");
+const paginationCallback = page => {
+  searchApi.page = page;
+  searchApi.searchMovies().then(handleSuccess);
+};
+const handleSuccess = ({ results, total_pages: totalPages }) => {
+  gallery.innerHTML = '';
+  renderMarkup(results);
+  storage.save(results);
+  pagination.totalPages = totalPages;
+};
+const searchFilms = e => {
+  gallery.innerHTML = '';
+  const query = e.currentTarget.elements.search.value.trim();
+  if (!query) {
+    return;
+  }
+  searchApi.query = query;
+  searchApi
+    .searchMovies()
+    .then(data => {
+      handleSuccess(data);
+      pagination.showPagination();
+    })
+    .catch(() => ERROR_MESSAGE);
 };
 
-
-submitForm.addEventListener("submit", searchFilms);
-
-// pagination---------------------------------------
-
-function paginationCallback(page) {
-    searchApi.page = page;
-    searchApi.searchMovies();
-}
+submitForm.addEventListener('submit', searchFilms);
