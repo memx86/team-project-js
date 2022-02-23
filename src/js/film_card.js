d4263735a296bd7d0b969e219c9b2ba2422058a3
Hanna Vasylenko <hanna.vasylenko05@gmail.com>
2022-02-23 17:25:47 +0200

    Add card styles

--- src/js/film_card.js
+++ src/js/film_card.js
@@ -1,10 +1,18 @@
+import Storage from './services/storage';
 import notFoundImg from '../images/film_card/poster-img.jpg';
 import notFoundImgRetina from '../images/film_card/poster-img@2x.jpg';
 const movieListRef = document.querySelector('.gallery');
 
+const genres = new Storage(Storage.KEYS.GENRES);
+const arr = genres.get();
+
 const makeMoviesMarkup = movies =>
   movies
-    .map(({ title, release_date, poster_path, genre_ids, vote_count }) => {
+    .map(({ title, release_date, poster_path, genre_ids, vote_average }) => {
+      const genresNames = genre_ids.map(id => {
+        const { name } = arr.find(item => item.id === id);
+        return name;
+      });
       const year = new Date(release_date).getFullYear();
       const poster2x = poster_path
         ? `https://image.tmdb.org/t/p/w500${poster_path}`
@@ -29,9 +37,9 @@ height="398"
 </picture>
 <h5 class="movie-title">${title}</h5>
 <div class="container_movie-info">
-<p class="movie-info">${genre_ids.join(', ')}</p>
-<p class="movie-info">${year}</p>
-<p class="movie-rating">${vote_count}</p>
+<p class="movie-info">${genresNames.join(', ')} |  </p>
+<p class="movie-info"> ${year} </p>
+<p class="movie-rating">${vote_average ? vote_average : 'N/A'}</p>
 </div>
 </div>
 </li>
