c2612caa9d9dfbbb88b65eb3cbaa64db0971e9b0
Yevhen Malyshko <memx86@gmail.com>
2022-03-16 23:17:29 +0200

    header buttons|input change animations, gallery render optimization

--- src/js/popular_movies.js
+++ src/js/popular_movies.js
@@ -1,13 +1,14 @@
 import { renderMarkup } from './templates/film_card';
 import { api, pagination } from './services';
 
-const gallery = document.querySelector('.gallery');
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
 
 async function popularMovies() {
   try {
-    gallery.innerHTML = ' ';
-    const { results, total_pages: totalPages } = await api.getTrending();
-    renderMarkup(results);
+    const totalPages = await renderMovies();
     pagination.page = api.page;
     pagination.totalPages = totalPages;
     pagination.callback = moreMovies;
@@ -17,12 +18,20 @@ async function popularMovies() {
   }
 }
 
+async function renderMovies() {
+  refs.loader.classList.remove('is-hidden');
+  const { results, total_pages: totalPages } = await api.getTrending();
+  refs.gallery.innerHTML = ' ';
+  renderMarkup(results);
+  refs.loader.classList.add('is-hidden');
+  return totalPages;
+}
+
 async function moreMovies() {
   try {
-    gallery.innerHTML = ' ';
     api.page = pagination.page;
-    const { results } = await api.getTrending();
-    renderMarkup(results);
+    await renderMovies();
+    refs.gallery.scrollIntoView();
   } catch (error) {
     console.log(error);
   }
